ad40f7f8f5040e6da4ef6f14be8c13cd56dc261c
rafaelGurgelS <rafaelgurgels@gmail.com>
2023-11-02 21:03:00 -0300

    adicionando vetores e separando em threads

--- home_assistant.py
+++ home_assistant.py
@@ -2,8 +2,14 @@ import pika
 import sys
 import os
 import socket
+import threading
+import time
 
-#global channel
+global LAMP 
+global AIR
+LAMP = False
+AIR = False
+
 
 def get_public_ip():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -29,8 +35,11 @@ class Home_assistant:
             3: "Bomba D'água"
         }
         self.server_socket=socket
-        self.channel_lamp=None
-        self.channel_air_cond=None
+
+        # Lista para armazenar mensagens
+        self.messagesLamp = []
+        self.messagesAir = []
+
         try:
             server_socket.bind((host, port))
         except socket.error:
@@ -40,47 +49,66 @@ class Home_assistant:
 
     def start(self):
         self.connect_to_client()
-        self.handle_devices()
-        self.start_communication()
+
+        lamp_thread = threading.Thread(target=self.handle_lamp)
+        air_cond_thread = threading.Thread(target=self.handle_air_conditioner)
+        send_messages_devices_thread = threading.Thread(target=self.send_messages_devices)
+        start_communication_thread = threading.Thread(target=self.start_communication)
+
+        lamp_thread.start()
+        air_cond_thread.start()
+        send_messages_devices_thread.start()
+        start_communication_thread.start()
 
 
     def lamp_callback(self, ch, method, properties, body):
-        ##eh aq q a gnt tem q enviar o ngc pro client
-        self.client_socket.send(f"ok:{body}".encode())
+        self.messagesLamp.append(body)
 
 
 
     def air_conditioner_callback(self,ch, method, properties, body):
-        self.client_socket.send(f"ok:{body}".encode())
+        self.messagesAir.append(body)
+
+
+    def send_messages_devices(self):
+        while True:
+            if LAMP:
+                if self.messagesLamp:
+                    message = self.messagesLamp.pop(0)
+                    combined_message = b"ok:" + message
+                    self.client_socket.send(combined_message)
+                    time.sleep(1)
+                else:
+                    pass
+
+            if AIR:
+                if self.messagesAir:
+                    message = self.messagesAir.pop(0)
+                    combined_message = b"ok:" + message
+                    self.client_socket.send(combined_message)
+                    time.sleep(1)
+                else:
+                    pass
+
+            
        
 
-    def handle_devices(self):
-        
-        connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost'))
-        print("conexao ok")
-
+    def handle_lamp(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         channel_lamp = connection.channel()
-        channel_air_cond=connection.channel()
-        
-        channel_lamp.exchange_declare(exchange='smart_lamp', exchange_type='fanout')
-        channel_air_cond.exchange_declare(exchange='air_conditioner', exchange_type='fanout')
-
         channel_lamp.queue_declare(queue='lamp_queue', exclusive=True)
-        channel_air_cond.queue_declare(queue='air_conditioner_queue', exclusive=True)
-
         channel_lamp.queue_bind(exchange='smart_lamp', queue='lamp_queue')
-        channel_air_cond.queue_bind(exchange='air_conditioner',queue='air_conditioner_queue')
+        channel_lamp.basic_consume(queue='lamp_queue', on_message_callback=self.lamp_callback, auto_ack=True)
+        channel_lamp.start_consuming()
 
-        print(' [*] Waiting for logs. To exit press CTRL+C')
+    def handle_air_conditioner(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel_air_cond = connection.channel()
+        channel_air_cond.queue_declare(queue='air_conditioner_queue', exclusive=True)
+        channel_air_cond.queue_bind(exchange='air_conditioner', queue='air_conditioner_queue')
+        channel_air_cond.basic_consume(queue='air_conditioner_queue', on_message_callback=self.air_conditioner_callback, auto_ack=True)
+        channel_air_cond.start_consuming()
 
-        #channel_lamp.basic_consume(queue='lamp_queue',
-                #     on_message_callback= self.lamp_callback, auto_ack=True)
-        channel_air_cond.basic_consume(queue='air_conditioner_queue',on_message_callback=self.air_conditioner_callback, auto_ack=True)
-        
-        self.channel_lamp=channel_lamp
-        self.channel_air_cond=channel_air_cond
-        print("ok aq")
     def connect_to_client(self):
         SERVER_IP=get_public_ip()
         SERVER_PORT=self.port
@@ -94,7 +122,7 @@ class Home_assistant:
         
         while True:
             device_options = "\n".join([f"{device_num} - {device_name}" for device_num, device_name in self.devices.items()])
-            menu = f"menu:Escolha um dispositivo:\n{device_options}\n0 - Sair\n"
+            menu = f"menu:\nEscolha um dispositivo:\n{device_options}\n0 - Sair\n"
             self.client_socket.send(menu.encode())
             choice = self.client_socket.recv(1024).decode()
 
@@ -107,15 +135,29 @@ class Home_assistant:
                 
                 device_num = int(choice)
                 if device_num in self.devices:
-                    self.client_socket.send(f"ok:Você escolheu {self.devices[device_num]}\n".encode())
+                    self.client_socket.send(f"ok:Você escolheu {self.devices[device_num]}\n Digite 0 para sair\n".encode())
                     # Lógica para lidar com o dispositivo escolhido
                     if device_num == 1:
                         # Lógica para Lâmpada
-                        self.channel_lamp.start_consuming()
+                        # Tem essa possibilidade de enviar tudo que tiver de uma vez, ou só mandar uma quantidade fixa
+                        #self.client_socket.send('\n'.join(self.messagesLamp).encode()) 
+                        #self.messagesLamp.clear()
+                        global LAMP
+                        LAMP = True
+                        while LAMP: 
+                            stop = self.client_socket.recv(1024).decode()
+                            if stop == "0":
+                                LAMP = False
+
                         
                     elif device_num == 2:
                         # Lógica para Ar condicionado
-                        self.channel_air_cond.start_consuming()
+                        global AIR
+                        AIR = True
+                        while AIR: 
+                            stop = self.client_socket.recv(1024).decode()
+                            if stop == "0":
+                                AIR = False
                     elif device_num == 3:
                         # Lógica para Bomba D'água
                         pass
@@ -133,7 +175,7 @@ if __name__ == '__main__':
     try:
         SERVER_IP=get_public_ip()
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        home_assistant = Home_assistant(SERVER_IP, 12345,server_socket)
+        home_assistant = Home_assistant(SERVER_IP, 12345, server_socket)
         home_assistant.start()
 
     except KeyboardInterrupt:
